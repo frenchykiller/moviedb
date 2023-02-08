45710f7b9826255ae1510b5cc0e4466ea79a43db
Christopher Walker <christophersvt3g@gmail.com>
2023-02-08 14:29:58 +0100

    Added Movie model and associated controller, route, component, and migration

---
+++ resources/views/components/movie-card.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <img src="{{ $poster }}" alt="{{ $title }}">
+    <div class="h1">{{ $title }}</div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MovieController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [MovieController::class, 'index'])->name('index');
 
 Route::middleware([
     'auth:sanctum',
